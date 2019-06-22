65999c37795598d9d8ab87ca7d86da1da4230937
zhushimao123 <152359342622@163.com>
2019-06-22 07:06:19 +0000

    dbug

--- app/Http/Controllers/user/LoginController.php
+++ app/Http/Controllers/user/LoginController.php
@@ -43,8 +43,10 @@ class LoginController extends Controller
     //通过code获取access_token
     public function code($code)
     {
+        var_dump($code);
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx48451c201710dbcd&secret=f583f90f3aed8ec33ae6dd30eceebe5f&code='.$code.'&grant_type=authorization_code';
         $json_data = file_get_contents($url);
+        var_dump($json_data);die;
         $arr_data = json_decode($json_data,true);
         var_dump($arr_data);die;
         $this-> accesstoken($arr_data['access_token']);
